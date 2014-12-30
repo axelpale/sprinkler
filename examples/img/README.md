d5e6f3290807bc05bc1e41811e47969f486a3be1
xeli <akseli.palen@gmail.com>
2014-12-31 00:51:04 +0200

    Snowfall example and first demo implementation.

---
+++ examples/img/README.md
@@ -0,0 +1,31 @@
+# Image origins and licenses
+
+<filename>
+<license>
+<origin url>
+<image>
+
+## snowflake.png
+CC0 Public domain
+http://www.publicdomainfiles.com/show_file.php?id=13920615011296
+![](snawflake.png)
+
+## snowflakeb.png
+CC0 Public domain
+http://www.publicdomainfiles.com/show_file.php?id=13527439218871
+![](snowflakeb.png)
+
+## 5cent.png
+CC0 Public domain
+http://www.publicdomainfiles.com/show_file.php?id=13925355011042
+![](5cent.png)
+
+## 2cent.png
+CC0 Public domain
+http://www.publicdomainfiles.com/show_file.php?id=13925350826546
+![](2cent.png)
+
+## 1cent.png
+CC0 Public domain
+http://www.publicdomainfiles.com/show_file.php?id=13925353811579
+![](1cent.png)
--- src/sprinkler.js
+++ src/sprinkler.js
@@ -22,7 +22,108 @@
   // ****************
   // Helper functions
   // ****************
-  //...
+
+  var loadImages = function (imgSrcs, then) {
+    // then(err, imgElements)
+    // Calls then after all the images were loaded.
+    var i, imgs, numberOfImages, onload, onloadsCalled;
+    numberOfImages = imgSrcs.length;
+
+    imgs = [];
+
+    onloadsCalled = 0;
+    onload = function () {
+      // Note:
+      //   this = Image
+      onloadsCalled += 1;
+      var isFinalImage = (onloadsCalled === numberOfImages);
+      if (isFinalImage) {
+        then(null, imgs);
+      }
+    };
+
+    for (i = 0; i < imgSrcs.length; i += 1) {
+      imgs.push(new Image());
+      imgs[i].onload = onload;
+      imgs[i].src = imgSrcs[i];
+    }
+  };
+
+  var randomIn = function (min, max) {
+    // Continuous uniform distribution.
+    // Return x: min <= x < max
+    var d = max - min;
+    return min + d * Math.random();
+  };
+
+  var randomPick = function (array) {
+    // Return one randomly picked element.
+    // Discrete uniform distribution.
+    var min = 0;
+    var max = array.length;
+    var continuousIndex = randomIn(min, max);
+    var discreteIndex = Math.floor(continuousIndex);
+    var i = discreteIndex;
+    return array[i];
+  };
+
+  var makeCanvasAutoFullwindow = function (canvas) {
+    // Canvas is resized when window size changes, e.g.
+    // when a mobile device is tilted.
+    //
+    // Parameter
+    //   canvas
+    //     HTML Canvas element
+    //
+    var resizeCanvas = function () {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+    };
+    // resize the canvas to fill browser window dynamically
+    window.addEventListener('resize', resizeCanvas, false);
+    // Initially resized to fullscreen.
+    resizeCanvas();
+  };
+
+  // TODO Remove useless parameters
+  var Particle = function (x, y, z, r, a, dx, dy, dz, dr, da,
+                           ddx, ddy, ddz, ddr, dda,
+                           img, w, h) {
+    this.x = x;
+    this.y = y;
+    this.z = z; // scale
+    this.r = r;
+    this.a = a; // opacity
+    this.dx = dx;
+    this.dy = dy;
+    this.dz = dz;
+    this.dr = dr;
+    this.da = da;
+    this.ddx = ddx;
+    this.ddy = ddy;
+    this.ddz = ddz;
+    this.ddr = ddr;
+    this.dda = dda;
+
+    this.img = img; // source image // read-only
+    this.w = w; // source image width // read-only
+    this.h = h; // source image height // read-only
+  };
+  Particle.prototype.tick = function (dt) {
+    // Parameter
+    //   dt
+    //     seconds
+    this.x += this.dx * dt;
+    this.y += this.dy * dt;
+    this.z += this.dz * dt;
+    this.r += this.dr * dt;
+    this.a += Math.min(1, Math.max(0, this.a + this.da * dt));
+    this.dx += this.ddx * dt;
+    this.dy += this.ddy * dt;
+    this.dz += this.ddz * dt;
+    this.dr += this.ddr * dt;
+    this.da += this.dda * dt;
+  };
 
 
 
@@ -37,12 +138,151 @@
   // Constructor
   // ***********
 
-  var Sprinkler = function () {
-    this.timeout = null;
+  var Sprinkler = function (canvas) {
+
+    // Images are modeled as particles and stored here
+    var particles = [];
+
+    // To pause animation loop
+    var running = false;
+
+    // number, unix timestamp milliseconds of most recent frame.
+    var past = null;
+
+    // Everything is drawn on canvas.
+    var ctx = canvas.getContext('2d');
+
+    // Make canvas resize automatically to full window area
+    makeCanvasAutoFullwindow(canvas);
+
+    // Model is simulated forward every frame
+    var tickModel = function (dt) {
+      // Parameter
+      //   dt
+      //     simulation time, seconds
+      var i, h, bufferParticles,visible, maxRadius;
+
+      // Simulate each particle
+      for (i = 0; i < particles.length; i += 1) {
+        particles[i].tick(dt);
+      }
+
+      // Update model: remove particles that are out of screen
+      // by replacing particles array.
+      h = canvas.height;
+      bufferParticles = [];
+      for (i = 0; i < particles.length; i += 1) {
+        maxRadius = Math.max(particles[i].w, particles[i].h) / 2;
+        visible = (particles[i].y < h + maxRadius);
+        if (visible) {
+          bufferParticles.push(particles[i]);
+        }
+      }
+      particles = bufferParticles;
+    };
+
+    // View is rendered each frame
+    var tickView = function () {
+      // Draw; View current model
+      var i, imgW, imgH;
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      for (i = 0; i < particles.length; i += 1) {
+        ctx.globalAlpha = particles[i].a;
+        imgW = particles[i].z * particles[i].w;
+        imgH = particles[i].z * particles[i].h;
+        ctx.translate(particles[i].x, particles[i].y);
+        ctx.rotate(particles[i].r);
+        ctx.drawImage(
+          particles[i].img,
+          -Math.floor(imgW / 2), // gravity to image center
+          -Math.floor(imgH / 2),
+          imgW, imgH
+        );
+        ctx.setTransform(1, 0, 0, 1, 0, 0); // resetTransform
+        ctx.globalAlpha = 1; // reset alpha
+      }
+    };
+
+    var startAnimationLoop = function loopFn() {
+      var present, dt;
+
+      // Time difference from previous frame in milliseconds
+      present = Date.now();
+      dt = (past === null) ? 0 : present - past;
+      past = present;
+
+      // Update Model
+      tickModel(dt / 1000); // secs
+
+      // Draw; View current model
+      tickView();
+
+      // Recursion
+      // Allow only one viewLoop recursion at a time.
+      if (running) {
+        window.requestAnimationFrame(loopFn);
+      }
+    };
+
+    var start = function () {
+      if (!running) {
+        running = true;
+        startAnimationLoop();
+      }
+    };
+
+    var stop = function () {
+      running = false;
+    };
+
+    var imagesToParticles = function (images) {
+      // Turn images to particles
+      var i, img, p, newParticles = [], w, h;
+      w = canvas.width;
+      h = canvas.height
+
+      for (i = 0; i < images.length; i += 1) {
+        img = images[i];
+
+        p = new Particle(
+          randomIn(0, w), // randomize start point
+          -Math.max(img.width, img.height) / 2, // maxRadius above canvas top
+          1, // z, scale
+          2 * Math.PI * randomIn(0, 1), // rotation
+          1, // a, alpha, opacity
+          randomIn(-1, 1), // dx, horizontal movement
+          randomIn(70, 100), // dy, falling speed
+          0, // dz
+          randomIn(-0.5, 0.5), // dr, rotation speed, rads/sec
+          0, // da
+          0, 0, // ddx, ddy
+          0, // ddz
+          0, // ddr
+          0, // dda
+          img,
+          img.width, img.height
+        );
+        newParticles.push(p);
+      }
+      return newParticles;
+    };
+
+    this.load = function (imagePaths, callback) {
+      // Parameter
+      //   imagePaths
+      //     array
+      //   callback
+      //     function (start, stop)
+      loadImages(imagePaths, function (err, images) {
+        if (err) { console.error(err); return; }
+        particles = imagesToParticles(images);
+        callback(start, stop);
+      });
+    };
   };
 
-  exports.create = function () {
-    return new Sprinkler();
+  exports.create = function (canvas) {
+    return new Sprinkler(canvas);
   };
 
 
@@ -50,18 +290,7 @@
   // ****************
   // Instance methods
   // ****************
-
-  Sprinkler.prototype.start = function () {
-    var interval = 100;
-    var that = this;
-    this.timeout = setTimeout(function () {
-      that.start();
-    }, interval);
-  };
-
-  Sprinkler.prototype.stop = function () {
-    clearTimeout(this.timeout);
-  };
+  // ...
 
 
 
