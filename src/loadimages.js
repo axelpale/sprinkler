d35ce8118cb5446b0ba0bb5a355916d0d940af85
Akseli Palén <akseli.palen@gmail.com>
2019-09-14 17:44:55 +0300

    Switch from jshint to standard and fix lint issues

--- src/loadimages.js
+++ src/loadimages.js
@@ -1,47 +1,46 @@
-module.exports = function loadImages(imgSrcs, then) {
+module.exports = function (imgSrcs, then) {
   // then(err, imgElements)
   // Calls then after all the images were loaded.
-  var i, imgs, numberOfImages, onload, onloadsCalled,
-    thereWasError, thereWasSuccess;
-  numberOfImages = imgSrcs.length;
-  thereWasSuccess = false;
-  thereWasError = false;
+  var numberOfImages = imgSrcs.length
+  var thereWasSuccess = false
+  var thereWasError = false
 
-  imgs = [];
+  var imgs = []
 
-  onloadsCalled = 0;
-  onload = function () {
+  var onloadsCalled = 0
+  var onload = function () {
     // Note:
     //   this = Image
     if (!thereWasError) {
-      onloadsCalled += 1;
-      var isFinalImage = (onloadsCalled === numberOfImages);
+      onloadsCalled += 1
+      var isFinalImage = (onloadsCalled === numberOfImages)
       if (isFinalImage) {
-        thereWasSuccess = true;
-        then(null, imgs);
+        thereWasSuccess = true
+        then(null, imgs)
       }
     }
-  };
+  }
 
-  onerror = function (errMsg) {
+  var onerror = function (errMsg) {
     // Note:
     //   this = Image
 
     // No errors after success.
     if (!thereWasSuccess) {
-      thereWasError = true;
-      then(errMsg, null);
+      thereWasError = true
+      then(errMsg, null)
     }
 
     // Prevent firing the default event handler
     // https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers.onerror#Parameters
-    return true;
-  };
-
-  for (i = 0; i < imgSrcs.length; i += 1) {
-    imgs.push(new Image());
-    imgs[i].onload = onload;
-    imgs[i].onerror = onerror;
-    imgs[i].src = imgSrcs[i];
+    return true
   }
-};
+
+  var i
+  for (i = 0; i < imgSrcs.length; i += 1) {
+    imgs.push(new window.Image())
+    imgs[i].onload = onload
+    imgs[i].onerror = onerror
+    imgs[i].src = imgSrcs[i]
+  }
+}
--- src/stat.js
+++ src/stat.js
@@ -1,33 +1,33 @@
 exports.randomIn = function (min, max) {
   // Continuous uniform distribution.
   // Return x: min <= x < max
-  var d = max - min;
-  return min + d * Math.random();
-};
+  var d = max - min
+  return min + d * Math.random()
+}
 
 exports.randomPick = function (array) {
   // Return one randomly picked element.
   // Discrete uniform distribution.
-  var min = 0;
-  var max = array.length;
-  var continuousIndex = exports.randomIn(min, max);
-  var discreteIndex = Math.floor(continuousIndex);
-  var i = discreteIndex;
-  return array[i];
-};
+  var min = 0
+  var max = array.length
+  var continuousIndex = exports.randomIn(min, max)
+  var discreteIndex = Math.floor(continuousIndex)
+  var i = discreteIndex
+  return array[i]
+}
 
 exports.samplePoisson = function (rate) {
   // Purpose: number of images to drop in each interval.
   // Take a sample from poisson distribution.
   // https://en.wikipedia.org/wiki/Poisson_distribution#Generating_Poisson-distributed_random_variables
-  var L, k, p, u;
-  L = Math.exp(-rate);
-  k = 0;
-  p = 1;
+  var L, k, p, u
+  L = Math.exp(-rate)
+  k = 0
+  p = 1
   do {
-    k += 1;
-    u = Math.random();
-    p *= u;
-  } while (p > L);
-  return k - 1;
-};
+    k += 1
+    u = Math.random()
+    p *= u
+  } while (p > L)
+  return k - 1
+}
