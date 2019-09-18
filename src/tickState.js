cd033b1629673350be9f9a4038ecbfe4a8a99c80
Akseli Palén <akseli.palen@gmail.com>
2019-09-18 22:00:15 +0300

    Replace thin tickState with contents of tickCanvas

--- src/tickState.js
+++ src/tickState.js
@@ -1,5 +1,20 @@
-var tickCanvas = require('./tickCanvas')
+var tickWave = require('./tickWave')
 
 module.exports = function (state, dt) {
-  tickCanvas(state, dt)
+  // Parameters
+  //   state
+  //   dt
+  //     simulation time, seconds
+  //
+
+  // To avoid huge wave of particles when the page gains focus after a while,
+  // the particles are created only if dt is close to given framerate.
+  if (dt > 0.5) {
+    return
+  }
+
+  // Simulate each wave. Each wave has its own options.
+  state.waves.forEach(function (wave) {
+    tickWave(state, wave, dt)
+  })
 }
