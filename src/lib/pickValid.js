2947bc95b23a24d97e36d900cb7f55ef4d4b6f7c
Akseli Palén <akseli.palen@gmail.com>
2019-10-01 02:17:55 +0300

    More robust pickValid

--- src/lib/pickValid.js
+++ src/lib/pickValid.js
@@ -5,19 +5,29 @@ module.exports = function (source, reference) {
   // A valid option exist in the reference object and is same type.
   //
   // Return
-  //   an object validated by the reference
+  //   a new object validated by the reference
   //
   var r = {}
   var k
-  for (k in reference) {
-    if (hasProp(reference, k)) {
-      r[k] = reference[k]
-      if (hasProp(source, k)) {
-        if (typeof source[k] === typeof reference[k]) {
-          r[k] = source[k]
+
+  if (typeof source === 'object') {
+    for (k in reference) {
+      if (hasProp(reference, k)) {
+        r[k] = reference[k]
+        if (hasProp(source, k)) {
+          if (typeof source[k] === typeof reference[k]) {
+            r[k] = source[k]
+          }
         }
       }
     }
+  } else {
+    for (k in reference) {
+      if (hasProp(reference, k)) {
+        r[k] = reference[k]
+      }
+    }
   }
+
   return r
 }
