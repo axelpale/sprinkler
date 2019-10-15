a2837c1a32b9c2b65e922849ff74d3cb72a5d70d
Akseli Palen <akseli.palen@gmail.com>
2019-10-15 20:54:51 +0300

    Fix #16 and #20: near to optimal position for the spawn line

--- src/model/createParticles.js
+++ src/model/createParticles.js
@@ -4,7 +4,12 @@ var hasProp = require('../lib/hasProp')
 var createTailParticles = require('./createTailParticles')
 var randomIn = stat.randomIn
 
-// We use this to add particles in to the model.
+var projectedWidth = function (angle, width, height) {
+  // The width of a rectangle when looked from a certain angle.
+  return Math.abs(width * Math.sin(angle)) + Math.abs(height * Math.cos(angle))
+}
+
+// We use this to add particles into the model.
 var createParticle = function (state, wave) {
   var opts = wave.options
 
@@ -17,7 +22,7 @@ var createParticle = function (state, wave) {
     imageUrl = stat.randomPick(wave.imageUrls)
   }
 
-  // Setup image object
+  // Setup Image object
   var image
   if (typeof imageUrl === 'string') {
     // Init or reuse image
@@ -37,22 +42,37 @@ var createParticle = function (state, wave) {
   var angle = opts.angle
   var w = state.canvas.width
   var h = state.canvas.height
+  var diag = projectedWidth(angle, w, h) // NOTE 0 angle here means to right.
 
-  // Use screen diagonal as distance from screen center.
-  // This leaves room for large sprites.
-  var radius = Math.sqrt(w * w, h * h)
+  // Find distance to the spawn line.
+  var radius
+  if (image.complete) {
+    // Known particle size.
+    // Spawn close to viewport border.
+    radius = opts.zMax * Math.max(image.width, image.height) + diag / 2
+  } else {
+    // Unknown particle size.
+    // Use screen diagonal as distance from screen center.
+    // This leaves room for large sprites.
+    radius = diag
+  }
 
+  // A vector from the canvas center to the spawn line center.
   // sin(0) = 0
   // cos(0) = 1
   // sin(270) = 0.6
   // cos(270) = -0.6
-
   var sn = Math.sin(angle)
   var cs = Math.cos(angle)
   var ax = radius * sn
   var ay = radius * -cs
 
-  // Define x distribution.
+  // The spawn line: a vector orthogonal to [ax, ay].
+  var splen = projectedWidth(angle + Math.PI / 2, w, h)
+  var spx = splen * -ay / radius
+  var spy = splen * ax / radius
+
+  // Define x-wise distribution.
   // Continuous or discrete.
   var unit
   var steps = opts.xSteps + 1 // +1 to correlate what is visible
@@ -62,12 +82,10 @@ var createParticle = function (state, wave) {
     unit = Math.random()
   }
 
-  // Use diagonal as rain width. OPTIMIZE use real minimal width.
-  // Take a point on orthogonal diagonal. Then a vector from its center.
-  // Enlarge the vector so that the rain is wide enough to fill corners.
+  // Pick a random point from the spawn line.
   var r = unit - 0.5 + opts.xOff
-  var bx = 1.5 * -ay * r
-  var by = 1.5 * ax * r
+  var bx = spx * r
+  var by = spy * r
 
   var rdx = randomIn(opts.dxMin, opts.dxMax)
   var rdy = randomIn(opts.dyMin, opts.dyMax)
@@ -75,8 +93,8 @@ var createParticle = function (state, wave) {
   var rddy = randomIn(opts.ddyMin, opts.ddyMax)
 
   return {
-    x: (w / 2) + ax + bx, // randomize start point
-    y: (h / 2) + ay + by,
+    x: w / 2 + ax + bx, // start point
+    y: h / 2 + ay + by,
     z: randomIn(opts.zMin, opts.zMax),
     r: angle + randomIn(opts.rMin, opts.rMax),
     a: randomIn(opts.aMin, opts.aMax),
